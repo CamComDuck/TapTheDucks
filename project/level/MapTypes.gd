a4e63fb8f70636e073e860b81fb8d491bcc82706
Camden Hovell <camden.hovell@bsu.edu>
2024-11-10 16:25:22 -0500

    Add maps 3 and 4 backgrounds and resources

--- project/level/MapTypes.gd
+++ project/level/MapTypes.gd
@@ -4,6 +4,12 @@ extends Resource
 @export var map_number : int
 @export var player_on_left : bool = true
 
+@export_category("Lane Directions")
+@export var lane_1_is_left_tree : bool = true
+@export var lane_2_is_left_tree : bool = true
+@export var lane_3_is_left_tree : bool = true
+@export var lane_4_is_left_tree : bool = true
+
 @export_category("Player Positions")
 @export var player_position_1 : Vector2
 @export var player_position_2 : Vector2
