2006b902109fbc9231ac60966fd77144798ed09b
Camden Hovell <camden.hovell@bsu.edu>
2024-11-12 11:54:44 -0500

    Add initial minigame animation to show player each incorrect spot

--- project/minigame/minigame.gd
+++ project/minigame/minigame.gd
@@ -5,10 +5,10 @@ signal minigame_finished (points_earned : int)
 var _player_positions : Array[Marker2D] = []
 var _hiding_spot_positions : Array[AnimatedSprite2D] = []
 
-var _allow_input := true
+var _allow_input := false
 var _player_current_spot := 0
 
-var _correct_spot : int
+var _correct_spot_index : int
 var _selected_spot : int
 
 @onready var player := $Player as Player
@@ -39,10 +39,12 @@ func _ready() -> void:
 	_hiding_spot_positions.append(hiding_spot_4)
 	_hiding_spot_positions.append(hiding_spot_5)
 	
-	_correct_spot = randi_range(0, _hiding_spot_positions.size() - 1)
-	finding_fruit.global_position = _hiding_spot_positions[_correct_spot].global_position
+	_correct_spot_index = randi_range(0, _hiding_spot_positions.size() - 1)
+	finding_fruit.global_position = _hiding_spot_positions[_correct_spot_index].global_position
 	finding_fruit.hide()
-	print(_correct_spot)
+	print("Correct duck: " + str(_correct_spot_index + 1))
+	await _show_wrong_shuffled_spots()
+	_allow_input = true
 	
 
 func _physics_process(_delta: float) -> void:
@@ -77,27 +79,46 @@ func _physics_process(_delta: float) -> void:
 		player.play_animation("minigame_pick")
 		
 
+func _show_wrong_shuffled_spots() -> void:
+	var shuffled_spots : Array[AnimatedSprite2D] = _hiding_spot_positions.duplicate()
+	shuffled_spots.remove_at(_correct_spot_index)
+	shuffled_spots.shuffle()
+	
+	for i in shuffled_spots:
+		await _on_reveal_spot(i, true, false)
+		
+
+func _on_reveal_spot(spot : AnimatedSprite2D, return_to_spot : bool, show_fruit : bool) -> void:
+	var revealed_position := Vector2(spot.global_position.x, spot.global_position.y - (GameInfo.grid_square_length * 2))
+	var tween_revealed : Tween = get_tree().create_tween()
+	tween_revealed.tween_property(spot,"global_position",revealed_position, 1).set_ease(Tween.EASE_OUT)
+	
+	await tween_revealed.finished
+	
+	if show_fruit: # Replace with showing fruit when tween is halfway done
+		finding_fruit.show()
+	
+	await create_tween().tween_interval(0.5).finished
+
+	if return_to_spot:
+		var return_position := Vector2(spot.global_position.x, spot.global_position.y + (GameInfo.grid_square_length * 2))
+		var tween_return : Tween = get_tree().create_tween()
+		tween_return.tween_property(spot,"global_position",return_position, 1).set_ease(Tween.EASE_OUT)
+		await tween_return.finished
+	
+		
+
+
 func _on_button_testing_pressed() -> void:
 	minigame_finished.emit(50)
 	queue_free()
 
 
 func _on_player_animation_finished() -> void:
-	if _selected_spot == _correct_spot:
-		finding_fruit.show()
-		
-	var chosen_position := Vector2(_hiding_spot_positions[_selected_spot].global_position.x, _hiding_spot_positions[_selected_spot].global_position.y - (GameInfo.grid_square_length * 2))
-	var tween_chosen : Tween = get_tree().create_tween()
-	tween_chosen.tween_property(_hiding_spot_positions[_selected_spot], "global_position",chosen_position, 0.25).set_ease(Tween.EASE_OUT)
-	await tween_chosen.finished
+	if _selected_spot == _correct_spot_index:
+		await _on_reveal_spot(_hiding_spot_positions[_selected_spot], false, true)
 	
-	if _selected_spot == _correct_spot:
-		pass
-		
 	else:
-		finding_fruit.show()
-		var correct_position := Vector2(_hiding_spot_positions[_correct_spot].global_position.x, _hiding_spot_positions[_correct_spot].global_position.y - (GameInfo.grid_square_length * 2))
-		var tween_correct : Tween = get_tree().create_tween()
-		tween_correct.tween_property(_hiding_spot_positions[_correct_spot], "global_position",correct_position, 0.25).set_ease(Tween.EASE_OUT)
-		await tween_correct.finished
+		await _on_reveal_spot(_hiding_spot_positions[_selected_spot], false, false)
+		await _on_reveal_spot(_hiding_spot_positions[_correct_spot_index], false, true)
 		
